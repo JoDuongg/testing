1891d423a39ed8f11bf8dc43569bda4d18e205ca
JoDuongg <sghao5906@gmail.com>
2022-12-14 21:08:01 -0500

    in

--- script.js
+++ script.js
@@ -115,3 +115,33 @@ function getRendererSize() {
   return [width, height];
 }
 
+//
+
+//var theCount;
+var alarm = document.getElementById("alarm");
+//var panel = document.getElementById("panel");
+//var turnOff = document.getElementById("turn-off");
+//var turnOffHor = document.getElementById("closing");
+//var detonate = document.getElementById("detonate");
+//var again = document.getElementById("again");
+//var panel2 = document.getElementById("panel2");
+alarm.volume = 1.0; //volume level
+
+
+
+
+
+
+
+var mute = document.getElementById("mute");
+mute.addEventListener("click", function () {
+	if (this.className == "muted") {
+    alarm.load();
+    alarm.play()
+		//alarm.muted = false;
+		this.classList.remove("muted");
+	} else {
+		alarm.muted = true;
+		this.classList.add("muted");
+	}
+});
